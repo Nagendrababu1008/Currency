179838b90a3573f23d4e7ed0602829175e97dc61
nagendrababu1008 <nagendrababu1008@gmail.com>
2022-09-12 18:58:58 +0530

    JpaRepocommit

--- currency-exchange-service/src/main/java/com/sysveda/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/sysveda/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -4,13 +4,13 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sysveda.microservices.currencyrepo.ExchangeValueRepository;
-
 @SpringBootApplication
 @RestController
 public class CurrencyExchangeController {
@@ -18,13 +18,15 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment environment;
 	
-	@Autowired
-	ExchangeValueRepository exchangeValueRepository;
+	@Autowired(required=true)
+	private ExchangeValueRepository exchangeValueRepository;
 
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
 
-		ExchangeValue exchangeValue =new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+//		ExchangeValue exchangeValue =new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		
+		ExchangeValue exchangeValue= exchangeValueRepository.findbyFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		return exchangeValue;
 
--- currency-exchange-service/src/main/java/com/sysveda/microservices/currencyrepo/ExchangeValueRepository.java
+++ currency-exchange-service/src/main/java/com/sysveda/microservices/currencyrepo/ExchangeValueRepository.java
@@ -1,9 +1,13 @@
 package com.sysveda.microservices.currencyrepo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.sysveda.microservices.currencyexchangeservice.ExchangeValue;
 
+@Component
 public interface ExchangeValueRepository extends JpaRepository<ExchangeValue,Long> {
 
+	ExchangeValue findbyFromAndTo(String from, String to);
 }
